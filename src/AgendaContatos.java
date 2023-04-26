4fd7567fc5c2c36a3653120c3bd3ee9f422e6fe3
GunNice <gunnicerca@gmail.com>
2023-04-25 21:30:09 -0300

    .

---
+++ src/AgendaContatos.java
@@ -0,0 +1,24 @@
+public class AgendaContatos {
+public String nome;
+public String sobreNome;
+public String telefoneContato;
+public String getNome() {
+    return nome;
+}
+public void setNome(String nome) {
+    this.nome = nome;
+}
+public String getSobreNome() {
+    return sobreNome;
+}
+public void setSobreNome(String sobreNome) {
+    this.sobreNome = sobreNome;
+}
+public String getTelefoneContato() {
+    return telefoneContato;
+}
+public void setTelefoneContato(String telefoneContato) {
+    this.telefoneContato = telefoneContato;
+}
+
+}
--- src/App.java
+++ src/App.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    Scanner leia = new Scanner(System.in);
+    int numeroContatos;
+    
+    System.out.println("Quantos contatos deseja Salvar ? ");
+    numeroContatos = leia.nextInt();
+
+    AgendaContatos [] lista = new AgendaContatos [numeroContatos];
+
+    for (int cont = 0; cont < numeroContatos; cont++) {
+          /* Limpa o terminal */
+              System.out.print("\033[H\033[2J");
+              System.out.flush();
+          /* Limpa o terminal */
+       
+    AgendaContatos contato = new AgendaContatos();
+    System.out.println("Informe o nome do contato:");
+    contato.setNome(leia.next());
+
+    System.out.println("Informe o sobrenome do contato:");
+    contato.setSobreNome(leia.next());
+
+    System.out.println("Informe Nº do contato:");
+    contato.setTelefoneContato(leia.next());
+
+    lista [cont] = contato;
     }
+     /* Limpa o terminal */
+     System.out.print("\033[H\033[2J");
+     System.out.flush();
+     /* Limpa o terminal */
+
+    System.out.println("---------- Seus Contatos Salvos -----------");
+    for (AgendaContatos agendaContatos : lista) {
+    
+        System.out.println( agendaContatos.getNome() + " " + agendaContatos.getSobreNome() + " " + agendaContatos.getTelefoneContato());
+    }
+ }
 }
